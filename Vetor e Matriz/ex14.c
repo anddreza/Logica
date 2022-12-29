6cdd63d4280e17cb204ffd4b738b00688b4c3197
Andreza Silva <77905947+anddreza@users.noreply.github.com>
2022-12-28 21:20:23 -0300

    Update ex14.c
    Consertei a lógica com alteração do nome das variáveis porque estavam me confundindo.

--- Vetor e Matriz/ex14.c
+++ Vetor e Matriz/ex14.c
@@ -11,26 +11,29 @@ em linhas distintas.
 *******************************************************************************/
 #include <stdio.h>
 int main(void){
-    int i, v[5], SAIDA[5]; 
-    for (i = 0; i < 5; i++){
-        printf("Digite o numero: ");
+    int v[5], m[5], n[5]; 
+    for (int i = 0; i < 5; i++){
+        printf("Digite o %i numero: ", i+1);
         scanf("%i", &v[i]);
         fflush(stdin);
-
-    if(v[i]%2 == 0){
-     // printf("\nO numero %i na posicao %i e PAR \n", v[i], i);
-        SAIDA[i] = 2 * v[i] -1;
-     
-    } else if(v[i] % 2 == 1) {
-       // printf("\nO numero %i na posicao %i e IMPAR\n", v[i], i);
-        SAIDA[i] = 2*v[i] -1;
-     
-    }
-}
-
-  printf("A saída dos numeros pares %i\n", SAIDA[i]);
- printf("A saida para os numeros impares e %i\n", SAIDA[i]);
-    for(i = 0; i < 5; i++){
+      // Porque fazer uma divisão no "i", acho que na questão ele não pede isso
+      // O "i" serve dentro de um "FOR" para saber em qual posição vc está
+    if(i % 2 == 0){
+      m[i] = 2 * v[i] - 1;
+    //  printf("\nO numero %i na posicao %i e PAR \n", v[i], i);
+      printf("O vetor m é %i \n", m[i]);
+    } else if(i % 2 == 1) {
+        n[i] = v[i] + 3;
+     // printf("\nO numero %i na posicao %i e IMPAR\n", v[i], i);
+       printf("O vetor n é %i \n", n[i]);
+      }
+  }
+  
+  //printf("A saída dos numeros pares %i\n", m[i]); // qual a posição do "i" que vc está olhando? TODAS, QUERO TODAS AS POSIÇÕES
+//  printf("A saida dos numeros impares e %i\n", n[i]); // qual a posição do "i" que vc está olhando? TODAS, QUERO TODAS AS POSIÇÕES
+    for(int i = 0; i < 5; i++){
+      printf("A saída dos numeros pares %i\n", m[i]);
+      printf("A saida dos numeros impares e %i\n", n[i]);
      // scanf("%i", SAIDA[i]);
     }
     return 0;
