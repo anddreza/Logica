8e00ed79b1d6c9b57b799a996aac133aa6f2e0e9
Andreza <dedezasilva@live.com>
2023-08-03 21:36:27 -0300

    feat: revisão de lógica em C

---
+++ Aula 1 - Estrutura de dados/test1.c
@@ -0,0 +1,24 @@
+//Um programa que contém duas variáveis inteiras e cada uma destas variáveis deve ter um valor atribuído. Escrever um programa que mostre os seguintes resultados:
+//a) A soma das duas variáveis.
+//b) A diferença entre as duas variáveis.
+//c) O dobro da primeira mais o triplo da segunda variável.
+//d) A multiplicação das duas variáveis.
+
+#include <stdio.h>
+#include <math.h>
+ 
+int soma(int x, int y); 
+
+int main(){
+	
+	//printf("A soma das duas variáveis: ");
+	int c = soma(1 , 2);
+	printf("%i", c);
+
+	return 0;
+}
+
+int soma(int x, int y){
+	int a = x + y;
+	return a;
+}
