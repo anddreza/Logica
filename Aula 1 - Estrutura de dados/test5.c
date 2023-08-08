c675326e83643a651bb748e1a03af51ebfd90241
Andreza <dedezasilva@live.com>
2023-08-08 11:47:59 -0300

    feat: resolução da questão 5

---
+++ Aula 1 - Estrutura de dados/test5.c
@@ -0,0 +1,11 @@
+/*5) Escrever um programa que incremente uma variável com valor mínimo 0 e valor máximo 100 e as mostre no vídeo.*/
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	for(int i = 0; i < 101; i++){
+		printf("%d \n", i);
+	}
+}
